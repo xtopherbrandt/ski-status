5a3751d0867a51437e5459c6229af49351af79c8
Christopher Brandt <xtopher.brandt@gmail.com>
2019-01-01 20:46:37 -0800

    fix default welcome

--- ski-status-webhook/index.js
+++ ski-status-webhook/index.js
@@ -76,11 +76,13 @@ module.exports = async function (azureContext, req) {
 
     app.intent('Default Welcome Intent', conv => {
         conv.ask('Hi, how is it going?')
+        /**
         conv.ask(`Here's a picture of a cat`)
         conv.ask(new Image({
         url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
         alt: 'A cat',
         }))
+        */
     })
     
     // Intent in Dialogflow called `Goodbye`
